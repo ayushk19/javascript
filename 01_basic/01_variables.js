2b7ff199d56470134a7fabc6ad4b87677926a6c8
Ayush Kesarwani <68319127+ayushk19@users.noreply.github.com>
2023-09-28 19:16:22 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const account_id = 12345
+let account_email = "ayu@mail.com"
+var account_password = "123445"
+account_city = "Chandigarh"
+
+// account_id = 23 // not allowed ( we can't change constant variables value )
+
+account_email = "ayush@gmail.com"
+account_password = "122"
+account_city = "Jaipur"
+
