cbfbb5240782eb482fa854bbe5319161df6846cb
Ayush Kesarwani <68319127+ayushk19@users.noreply.github.com>
2023-09-29 10:55:37 +0000

    datatypes

--- 01_basic/02_Datatypes.js
+++ 01_basic/02_Datatypes.js
@@ -2,3 +2,24 @@
 
 // alert(3 + 3) // we are using nodejs not browser
 
+console.log("Ayush")
+let name = "Ayush"  //string
+let age = 20        //integer  
+let isLoggedIn = true   //boolean
+
+// premitive datatypes
+
+//number => 2 is to power 53
+// bigInt
+//string = ""
+// boolean => true/false
+// null => standalone value
+//undefined => 
+// symbol => unique
+
+//object
+
+// typeof // used to find type of any variable
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //object
