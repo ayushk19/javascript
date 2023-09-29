6582cdbed22ecabc5299bc86b51c3c16d4596c46
Ayush Kesarwani <68319127+ayushk19@users.noreply.github.com>
2023-09-29 11:11:35 +0000

    datatypes coversion

---
+++ 01_basic/03_datatypes_conversion.js
@@ -0,0 +1,29 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score)); //both are same
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "98" => 98
+// "98avbv" => NaN
+// true => 1; false => 0
+// undefined => NaN
+// null => 0
+
+let loggedIn = "Ayush"
+
+let booleanLoggedIn = Boolean(loggedIn)
+// console.log(booleanLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false; " " => true
+// "Ayush" => true
+
+let somenumber = 88
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
