21acf33601928040dde4cfb79c65dc1d270638ad
Ayush Kesarwani <68319127+ayushk19@users.noreply.github.com>
2023-09-29 10:44:23 +0000

    datatypes

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,6 +2,7 @@ const account_id = 12345;
 let account_email = "ayu@mail.com"
 var account_password = "123445"
 account_city = "Chandigarh"
+let account_state; // prints undefined if we can't assign anything
 
 // account_id = 23 // not allowed ( we can't change constant variables value )
 
@@ -9,4 +10,9 @@ account_email = "ayush@gmail.com"
 account_password = "122"
 account_city = "Jaipur"
 
-console.table([account_id, account_email, account_password, account_city])
+/**
+ * prefer not to use var
+ * because of issue in block scope and functional scope
+ */
+
+console.table([account_id, account_email, account_password, account_city, account_state])
