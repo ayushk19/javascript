a528d3474ddc9f9993c71222ac03e5eb458e3111
Ayush Kesarwani <68319127+ayushk19@users.noreply.github.com>
2023-10-03 19:55:54 +0000

    datatypes_stack_heap

--- 01_basic/datatypes_summary.js
+++ 01_basic/datatypes_summary.js
@@ -29,5 +29,29 @@ const myFunction = function(){
     console.log("Hello! World");
 }
 
-console.log(typeof myFunction)  // function
-console.log(typeof cars)    //Object
+// console.log(typeof myFunction)  // function
+// console.log(typeof cars)    //Object
+
+// ****************************************************
+
+// Stack (Primitive) Call by value 
+// Heap (Non primitive) call by reference
+
+let myname = "Ayush"
+
+let anothername = myname
+anothername = "Ayu"
+
+console.log(myname);
+console.log(anothername);
+
+let user = {
+    email: "user@mail.com",
+    upi: "UPIa@ybl"
+}
+
+let userSecond = user
+userSecond.email = "ayu@mail.com"
+
+console.log(user.email);
+console.log(userSecond.email);
